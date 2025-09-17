09dba1886e22a2e17d6d8fe6dafddf00963a1318
PIEDLA <luis.piedrahita@iefedericoozanam.edu.co>
2025-09-17 14:51:54 -0500

    Cambios 17/09/2025

--- Taller/lib_repositorios/Interfaces/IDetalles_PagoApliacion.cs
+++ Taller/lib_repositorios/Interfaces/IDetalles_PagoApliacion.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using lib_dominio.Entidades;
 
 namespace lib_repositorios.Interfaces
 {
-    internal class FileName
+    public interface IDetalles_PagoAplicacion
     {
+        void Configurar(string StringConexion);
+        List<Detalles_Pago> PorEstudiante(Detalles_Pago? entidad);
+        List<Detalles_Pago> Listar();
+        Detalles_Pago? Guardar(Detalles_Pago? entidad);
+        Detalles_Pago? Modificar(Detalles_Pago? entidad);
+        Detalles_Pago? Borrar(Detalles_Pago? entidad);
     }
 }
