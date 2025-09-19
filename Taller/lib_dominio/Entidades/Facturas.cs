45dfb15f1c98a2d9beb1224989a867c4090fea6a
maleja <marialagos321259@correo.itm.edu.co>
2025-09-18 19:07:47 -0500

    cambios

--- Taller/lib_dominio/Entidades/Facturas.cs
+++ Taller/lib_dominio/Entidades/Facturas.cs
@@ -13,7 +13,7 @@ namespace lib_dominio.Entidades
         [ForeignKey("Id_cliente")] public Clientes? _Cliente { get; set; }
         [ForeignKey("Id_reparacion")] public Reparaciones? _Reparacion { get; set; }
 
-        public List<Pagos>? Pagos { get; set; }
+        public List<Pagos>? Pagos;
         public List<Detalles_Producto>? Detalles_Producto;
         public List<Detalles_Servicio>? Detalles_Servicio;
         public List<Detalles_Repuesto>? Detalles_Repuesto;
